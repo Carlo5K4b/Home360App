c1c46ecfe62a7ba874f182c822aec4d5f815aecb
Carlos Beltrán <minimalkab@gmail.com>
2025-05-18 12:55:08 -0500

    Change message validation

--- src/main/java/com/pragma/home360/app/dto/request/CategoryRequest.java
+++ src/main/java/com/pragma/home360/app/dto/request/CategoryRequest.java
@@ -3,5 +3,5 @@ package com.pragma.home360.app.dto.request;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
-public record CategoryRequest(@NotNull(message = "no puede ser vacio") @NotBlank String name, @NotNull @NotBlank String description) {
+public record CategoryRequest(@NotNull(message = "no puede ser nulo - test message") @NotBlank String name, @NotNull @NotBlank String description) {
 }
